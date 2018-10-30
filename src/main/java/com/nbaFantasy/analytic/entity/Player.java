aad418af4082607d1f8dd72b94da1ccb06b3cd9f
Ashwin Shenoy <ashenoy1@binghamton.edu>
2018-10-30 00:22:23 -0400

    Added PLAYER bean

---
+++ src/main/java/com/nbaFantasy/analytic/entity/Player.java
@@ -0,0 +1,30 @@
+package com.nbaFantasy.analytic.entity;
+
+public class Player {
+    /*
+    personId	:	201142
+teamId	:	1610612744
+isOnCourt	:	false
+points	:	14
+pos	:	SF
+min	:	28:08
+fgm	:	5
+fga	:	9
+fgp	:	55.6
+ftm	:	3
+fta	:	3
+ftp	:	100
+tpm	:	1
+tpa	:	4
+tpp	:	25.0
+offReb	:	0
+defReb	:	3
+totReb	:	3
+assists	:	8
+pFouls	:	2
+steals	:	1
+turnovers	:	0
+blocks	:	1
+plusMinus	:	45
+     */
+}
